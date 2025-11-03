29dd3cab30959b91c49e3a195f596fc42697332a
local-user <local@user.com>
2025-11-03 22:17:24 +0100

    fix: moved start ngrok code to python file

--- flask_app/ngrok_service.py
+++ flask_app/ngrok_service.py
@@ -1,6 +1,13 @@
 from pyngrok import ngrok
+from flask_app.app import app
+
 
 def start_ngrok(port):
     public_url = ngrok.connect(port)
     print(f"Ngrok URL: {public_url}")
     return public_url
+
+if __name__ == "__main__":
+    public_url = start_ngrok(5000)
+    print(f"Access your app at: {public_url}")
+    app.run(host="0.0.0.0", port=5000)
