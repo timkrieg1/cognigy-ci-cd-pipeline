72057d83d3ac928a9c224dda3fbc69efa60bb5cc
local-user <local@user.com>
2025-11-16 12:17:09 +0100

    fix: removed non existant functions

--- helper_files/extract_feature_branch_agent.py
+++ helper_files/extract_feature_branch_agent.py
@@ -1,5 +1,5 @@
 from cognigy_client import CognigyAPIClient
-from helper_functions import read_json_files_in_directory, replace_ids_in_json_files, replace_ids_in_feature_directory
+from helper_functions import replace_ids_in_feature_directory
 from dotenv import load_dotenv
 import os
 import sys
@@ -97,12 +97,12 @@ CognigyAPIClientFeature.extract_agent_resources_by_ids(
 # --- Replace the feature bot specific ids with the original ids of the main agent ---
 replace_ids_in_feature_directory(agent_folder, feature_agent_folder, feature_branch_agent_id, main_branch_agent_id)
 
-""" # --- Replace the agent folder with the feature_agent folder ---
+# --- Replace the agent folder with the feature_agent folder ---
 if os.path.exists(agent_folder):
     shutil.rmtree(agent_folder)  # Remove the existing agent folder
 shutil.copytree(feature_agent_folder, agent_folder)  # Copy feature_agent to agent
 shutil.rmtree(feature_agent_folder)  # Remove the feature_agent folder after copying
-print(f"Replaced the '{agent_folder}' folder with the contents of the '{feature_agent_folder}' folder.") """
+print(f"Replaced the '{agent_folder}' folder with the contents of the '{feature_agent_folder}' folder.")
 
 # --- Git branch validation and commit logic ---
 try:
